48ae7f03b3a337125afd71d116cdf0ce2838e32b
szorfein <szorfein@protonmail.com>
2020-04-30 00:02:50 +0200

    add config file

--- lib/spior/install.rb
+++ lib/spior/install.rb
@@ -6,13 +6,17 @@ module Spior
   class Install
 
     def self.dependencies
-      if Pathname.new("/usr/bin/emerge") then
+      if Pathname.new("/usr/bin/emerge")
         puts "Install with emerge..."
         system('sudo emerge -av --changed-use tor iptables')
 
-      elsif Pathname.new("/usr/bin/pacman") then
+      elsif Pathname.new("/usr/bin/pacman")
         puts "Install with pacman..."
         system('sudo pacman -S --needed tor iptables')
+
+      elsif Pathname.new("/usr/bin/apt-get")
+        puts "Install with apt-get"
+        system('sudo apt-get tor iptables')
       end
     end
 
